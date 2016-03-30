4c41245b5d664997f9ab0c69f2de50f3c9eb6f8d
VEGAS <wzwrwj@163.com>
2016-03-30 15:08:39 +0800

    cnn 0.978

--- Digit Recognizer/CNN.py
+++ Digit Recognizer/CNN.py
@@ -22,4 +22,75 @@ test = np.array(test).reshape((-1, 1, 28, 28)).astype(np.uint8)
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+#print train[1729][0]
 plt.imshow(train[1729][0]) # draw the picture
+import lasagne
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+#from nolearn.lasagne import visualize
+
+net1 = NeuralNet(
+        layers=[('input', layers.InputLayer),
+                ('hidden', layers.DenseLayer),
+                ('output', layers.DenseLayer),
+                ],
+        # layer parameters:
+        input_shape=(None,1,28,28),
+        hidden_num_units=1000, # number of units in 'hidden' layer
+        output_nonlinearity=lasagne.nonlinearities.softmax,
+        output_num_units=10,  # 10 target values for the digits 0, 1, 2, ..., 9
+
+        # optimization method:
+        update=nesterov_momentum,
+        update_learning_rate=0.0001,
+        update_momentum=0.9,
+
+        max_epochs=15,
+        verbose=1,
+        )
+# Train the network
+#net1.fit(train, target)
+
+def CNN(n_epochs):
+    net1 = NeuralNet(
+        layers=[
+        ('input', layers.InputLayer),
+        ('conv1', layers.Conv2DLayer),      #Convolutional layer.  Params defined below
+        ('pool1', layers.MaxPool2DLayer),   # Like downsampling, for execution speed
+        ('conv2', layers.Conv2DLayer),
+        ('pool2', layers.MaxPool2DLayer),   # Like downsampling, for execution speed
+        ('hidden3', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+
+    input_shape=(None, 1, 28, 28),
+    conv1_num_filters=7,
+    conv1_filter_size=(3, 3),
+    conv1_nonlinearity=lasagne.nonlinearities.rectify,
+
+    pool1_pool_size=(2, 2),
+    pool2_pool_size=(2, 2),
+
+    conv2_num_filters=12,
+    conv2_filter_size=(2, 2),
+    conv2_nonlinearity=lasagne.nonlinearities.rectify,
+
+    hidden3_num_units=1000,
+    output_num_units=10,
+    output_nonlinearity=lasagne.nonlinearities.softmax,
+
+    update_learning_rate=0.0001,
+    update_momentum=0.9,
+
+    max_epochs=n_epochs,
+    verbose=1,
+    )
+    return net1
+
+cnn = CNN(300).fit(train,target) # train the CNN model for 15 epochs
+# use the NN model to classify test data
+pred = cnn.predict(test)
+
+# save results
+np.savetxt('output/submission_cnn_2.csv', np.c_[range(1,len(test)+1),pred],delimiter=',', header = 'ImageId,Label', comments = '', fmt='%d')
